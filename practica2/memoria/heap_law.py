35bd0b37d7f6b1afe6128822635b00d45075900a
pablomm <pablo.marcosm@estudiante.uam.es>
2019-03-12 17:14:49 +0100

    Arreglada ley de heap

--- practica2/memoria/heap_law.py
+++ practica2/memoria/heap_law.py
@@ -8,22 +8,15 @@ def func_powerlaw(x, K, b):
     return (x**b) * K
 
 
-
-file = "./heapLaw10k.txt"
+file = "./heap10k.txt"
 title = "docs10k"
-n_outliers = 0
-
 
 if __name__ == "__main__":
     data = np.loadtxt(file)
 
-    # Quitamos puntos aislados que distorsionan la grafica
-    for i in range(n_outliers):
-        idx = np.argmax(data, axis=0)
-        data[idx] = data[0]
 
-    x = data[:, 1]
-    y = data[:, 0]
+    x = data[:, 0]
+    y = data[:, 1]
 
     sol = curve_fit(func_powerlaw, x, y, maxfev=2000)[0]
 
@@ -31,11 +24,11 @@ if __name__ == "__main__":
     t = np.linspace(0, np.max(x), 1000)
 
 
-    plt.scatter(x, y)
+    plt.plot(x, y)
     label = "$" + str(round(sol[0],2)) +"x^{" + str(round(sol[1],2)) + "}$"
     plt.plot(t, func_powerlaw(t, sol[0], sol[1]), c="red", linestyle="--", label=label)
     plt.title(title)
-    plt.ylabel("Longitud del documento (#palabras)")
     plt.xlabel("#Tokens")
+    plt.ylabel("#Palabras")
     plt.legend()
     plt.show()
