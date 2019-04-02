cd45ac9f22951d4642f257e528081281d50177b1
Laseario <miguel.laseca@estudiante.uam.es>
2019-04-02 14:14:25 +0200

    Nothing important

--- practica3/src/es/uam/eps/bmi/search/test/Crawling.java
+++ practica3/src/es/uam/eps/bmi/search/test/Crawling.java
@@ -8,7 +8,7 @@ import es.uam.eps.bmi.search.index.lucene.LuceneBuilder;
 
 public class Crawling {
 
-	public static void main(String[] args) throws IOException {
+	public static void main (String a[]) throws IOException {
 		
 		String seedFile = "collections/urls.txt";
 		String indexPath = "crawler/urls";
@@ -17,10 +17,12 @@ public class Crawling {
 		IndexBuilder builder = new LuceneBuilder();
 		WebCrawler crawler = new WebCrawler(builder, 100, 10);
 		
+		long startTime = System.nanoTime();
 		crawler.build(seedFile, indexPath);
+		long endTime = System.nanoTime();
 		
-		System.out.println("Se han indexado " + crawler.getNumDocs() + " documentos.");
-
+		System.out.println("Se han indexado " + crawler.getNumDocs() + " documentos en "
+				+ (endTime-startTime)/1000000 + " milisegundos.");
 	}
 
 }
