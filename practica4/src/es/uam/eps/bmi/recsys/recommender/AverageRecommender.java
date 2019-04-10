40ec2339a3e34d6ab96f11e2f8bfb9f87026674d
Laseario <miguel.laseca@estudiante.uam.es>
2019-04-10 18:11:14 +0200

    kNN a medio hacer

--- practica4/src/es/uam/eps/bmi/recsys/recommender/AverageRecommender.java
+++ practica4/src/es/uam/eps/bmi/recsys/recommender/AverageRecommender.java
@@ -1,5 +1,6 @@
 package es.uam.eps.bmi.recsys.recommender;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 import es.uam.eps.bmi.recsys.Recommendation;
@@ -9,14 +10,31 @@ import es.uam.eps.bmi.recsys.ranking.RankingImpl;
 
 public class AverageRecommender implements Recommender {
 
-	double min;
 	Ratings rat;
+	// List with the items with ratings over the specified minimum
+	ArrayList<Integer> itemsList=new ArrayList<Integer>();
 	
 	public AverageRecommender(Ratings ratings,double min) {
-		this.min=min;
 		rat=ratings;
+		
+		// Fill the items list with those who satisfy the minimum rating
+		for (int item : ratings.getItems()) {
+			double average=0.0;
+			Set<Integer> users = rat.getUsers(item);
+			
+			// Calculate the average score of the item
+			for (int user : users)
+				average+=rat.getRating(user, item);
+			average=average/users.size();
+			
+			// Add the item if it passes the filter
+			if (average>=min) itemsList.add(item);
+		}
 	}
 	
+	/**
+	 * Not so sure if this is what this method is meant to do
+	 */
 	@Override
 	public Recommendation recommend(int cutoff) {
 		
@@ -26,7 +44,9 @@ public class AverageRecommender implements Recommender {
 			// We create and populate each user's corresponding ranking
 			RankingImpl ranking = new RankingImpl(cutoff);
 			for (int item : rat.getItems(user))
-				ranking.add(item, rat.getRating(user, item));
+				// Filter the items that satisfy the minimum rating 
+				if (itemsList.contains(item))
+					ranking.add(item, rat.getRating(user, item));
 			// The ranking is added to the return
 			recommendation.add(user, ranking);
 		}
--- practica4/src/es/uam/eps/bmi/recsys/recommender/UserKNNRecommender.java
+++ practica4/src/es/uam/eps/bmi/recsys/recommender/UserKNNRecommender.java
@@ -3,27 +3,73 @@ package es.uam.eps.bmi.recsys.recommender;
 import java.util.HashMap;
 
 import es.uam.eps.bmi.recsys.Recommendation;
+import es.uam.eps.bmi.recsys.data.Ratings;
+import es.uam.eps.bmi.recsys.ranking.Ranking;
+import es.uam.eps.bmi.recsys.ranking.RankingElement;
+import es.uam.eps.bmi.recsys.ranking.RankingImpl;
+import es.uam.eps.bmi.recsys.recommender.similarity.Similarity;
 
 public class UserKNNRecommender implements Recommender {
 	
-	// off-line neighbourhood checking  
-	HashMap<Integer,HashMap<Integer,Double>> neighbourhood=
-			new HashMap<Integer,HashMap<Integer,Double>> ();
+	// off-line neighborhood checking  
+	HashMap<Integer,Ranking> neighborhood=
+			new HashMap<Integer,Ranking> ();
+	
+	// Rankings of the users
+	Ratings ratings;
+	
+	// Name of the similarity used
+	String sim = null;
 
-	public UserKNNRecommender () {
+	public UserKNNRecommender (Ratings rat, Similarity s, int k) {
 		
+		ratings=rat;
+		// Get the similarity
+		sim=s.toString();
+		
+		// Populate the neighborhood with each user's ranking
+		for (int user1 : ratings.getUsers()) {
+			// Rank the other users based on their similarity to the first one
+			RankingImpl ranking = new RankingImpl(k);
+			for (int user2 : ratings.getUsers())
+				if (user1!=user2) ranking.add(user2, s.sim(user1, user2));
+			// Add the ranking
+			neighborhood.put(user1,ranking);
+		}
 	}
 	
 	@Override
-	public Recommendation recommend(int cutoff) {
+	public Recommendation recommend(int cutoff) {		
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public double score(int user, int item) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		double score=0;
+		
+		// Variable that stores the sum of all neighbors' similarities
+		double c_sum=0;
+		
+		// Iterate through the k-NN to user
+		for (RankingElement rank : neighborhood.get(user)) {
+			// Get the similarity and the ID of the user
+			double sim=rank.getScore();
+			int user_aux=rank.getID();
+			
+			// c=1/sum(sim(u,v), forall v in kNN(u))
+			c_sum+=sim;
+			// The initial score is the sum of the products of the similarities
+			// and the rating the neighbor gave to item
+			score+=sim*ratings.getRating(user_aux, item);
+		}
+		
+		// r^= c * sum(rating(v,i)*sim(u,v), forall v in kNN(u))
+		return score/c_sum;
 	}
 
+	public String toString() {
+		return "user-based kNN ("+ sim +")";
+	}
 }
